86a811ca98f7e94fb0bc42d36ebdaae534b296f2
NIkolai Taradanov <n.taradanov@gmail.com>
2021-09-01 10:52:15 +0700

    Заполнение данными

--- data.sql
+++ data.sql
@@ -6,5 +6,118 @@ values ('Пятерочка', '5@ru'),
        ('DNS', 'manager@dns.ru'),
        ('F5', 'user@f5.ru');
 
-insert into orders(client_id)
-values (2)
+insert into warehouse(name)
+values ('Основной'),
+       ('Офис'),
+       ('Брак');
+
+insert into type_of_goods(name)
+values ('Процессоры'),
+       ('Видеокарты'),
+       ('Велосипеды');
+# todo При удалении вида номенклатуры  проверять что нет подчиненных,
+#  причем если нет номенклатуры-давать удалять, если есть номенклатура-не удалять
+
+insert into spec_type_of_goods(name, type_of_goods_id)
+values ('Производитель', 1),
+       ('Семейство', 1),
+       ('Количество ядер', 1),
+       ('Производитель', 2),
+       ('Производитель', 3),
+       ('Назначение', 3);
+/*
+# select spec_type_of_goods.id,spec_type_of_goods.name,tog.name,tog.id
+# from spec_type_of_goods
+#          left join type_of_goods tog on tog.id = spec_type_of_goods.type_of_goods_id
+# order by type_of_goods_id, spec_type_of_goods.id;
+*/
+# Видеокарты Производитель
+insert into values_of_spec_type(spec_type_of_good_id, `value`)
+values (4, 'ASUS'),
+       (4, 'Gigabyte'),
+       (4, 'MSI');
+
+insert into values_of_spec_type(spec_type_of_good_id, `value`)
+values (5, 'Outleap'),
+       (5, 'GT'),
+       (5, 'Canondale');
+
+insert into values_of_spec_type(spec_type_of_good_id, `value`)
+values (6, 'MTB'),
+       (6, 'BMG'),
+       (6, 'Free Ride');
+
+insert into values_of_spec_type(spec_type_of_good_id, `value`)
+values (1, 'AMD'),
+       (1, 'Intel'),
+       (1, 'Cyrix');
+
+insert into values_of_spec_type(spec_type_of_good_id, `value`)
+values (2, 'Ryzen 5000'),
+       (2, 'Caby lake'),
+       (2, 'Ryzen 3000'),
+       (2, 'Bubbleshift');
+
+insert into values_of_spec_type(spec_type_of_good_id, `value`)
+values (3, '2'),
+       (3, '4'),
+       (3, '8');
+
+# select * from type_of_goods;
+
+insert into goods (id, type_of_goods_id, `name`) value (null, 1, 'Домашний процессор');
+
+insert into goods_description(goods_id, spec_type_of_good_id, values_of_spec_type_id) value (1, 1, 10);
+insert into goods_description(goods_id, spec_type_of_good_id, values_of_spec_type_id) value (1, 2, 16);
+insert into goods_description(goods_id, spec_type_of_good_id, values_of_spec_type_id) value (1, 3, 18);
+
+insert into goods (id, type_of_goods_id, `name`) value (null, 1, 'Офисный процессор');
+
+insert into goods_description(goods_id, spec_type_of_good_id, values_of_spec_type_id) value (2, 1, 11);
+insert into goods_description(goods_id, spec_type_of_good_id, values_of_spec_type_id) value (2, 2, 14);
+insert into goods_description(goods_id, spec_type_of_good_id, values_of_spec_type_id) value (2, 3, 19);
+
+insert into goods (id, type_of_goods_id, `name`) value (null, 2, 'Видеокарта 1050ТИ');
+
+insert into goods_description(goods_id, spec_type_of_good_id, values_of_spec_type_id) value (3, 4, 1);
+
+insert into goods (id, type_of_goods_id, `name`) value (null, 2, 'Видеокарта 6600');
+
+insert into goods_description(goods_id, spec_type_of_good_id, values_of_spec_type_id) value (4, 4, 2);
+
+insert into goods (id, type_of_goods_id, `name`) value (null, 3, 'NineWave');
+
+insert into goods_description(goods_id, spec_type_of_good_id, values_of_spec_type_id) value (5, 5, 4);
+insert into goods_description(goods_id, spec_type_of_good_id, values_of_spec_type_id) value (5, 6, 7);
+
+insert into goods (id, type_of_goods_id, `name`) value (null, 3, 'Block Gripper');
+
+insert into goods_description(goods_id, spec_type_of_good_id, values_of_spec_type_id) value (6, 5, 6);
+insert into goods_description(goods_id, spec_type_of_good_id, values_of_spec_type_id) value (6, 6, 9);
+# todo Вьюха остатки товара на складах
+/*
+# select tog.name,
+#        tog.id,
+#        stog.name,
+#        stog.id,
+#        vost.value,
+#        vost.id
+# from type_of_goods tog
+# left join spec_type_of_goods stog on tog.id = stog.type_of_goods_id
+# left join values_of_spec_type vost on stog.id = vost.spec_type_of_good_id
+# order by tog.id;
+*/
+# Брак
+insert into leftover_goods(warehouse_id, goods_id, received, shipped, data) value (3, 6, 1, 0, '2021-01-30 00:00:00');
+
+# Офис
+insert into leftover_goods(warehouse_id, goods_id, received, shipped, data)
+values (2, 2, 5, 0, '2021-02-30 00:00:00'),
+       (2, 2, 0, 2, '2021-02-28 00:00:00'),
+       (2, 2, 0, 1, '2021-03-30 00:00:00');
+
+# Основной склад
+insert into leftover_goods(warehouse_id, goods_id, received, shipped, data)
+values (1, 2, 5, 0, '2021-02-30 00:00:00'),
+       (1, 2, 0, 2, '2021-02-28 00:00:00'),
+       (1, 2, 0, 1, '2021-03-30 00:00:00');
--- raw.sql
+++ raw.sql
@@ -1,71 +1,3 @@
-/*drop schema if exists magazin;
-
-create schema if not exists magazin default character set utf8mb4 collate utf8mb4_0900_ai_ci;
-
-use magazin;
-
-#  лог файл
-create table if not exists log_s
-(
-# todo въюха,
-# todo процедуры логирования записи на изменение состава заказа
-
-    create_datetime datetime default now() not null,
-    table_name      varchar(25)            not null,
-    id              int unsigned           not null,
-    description     varchar(255)
-) engine = 'Archive';
-
-create table if not exists storehouses
-(
-    id   int unsigned not null auto_increment,
-    name varchar(25)  not null,
-    primary key (id)
-);
-insert into storehouses(name)
-values ('Офис'),
-       ('Основной');
-
-create table if not exists clients
-(
-    id    int unsigned not null auto_increment,
-    name  varchar(100) not null,
-    email varchar(100) not null,
-    primary key (id)
-#     unique index 'email_UNIQUE' ('email' ASC)
-) engine InnoDB;
-
-alter table clients
-    ADD unique index email_UNIQUE (email asc);
-
-create table if not exists orders
-(
-    id        int unsigned not null auto_increment,
-    client_id int unsigned not null,
-    amount    int unsigned default 0,
-    primary key (id),
-    CONSTRAINT `fk_orders_clients`
-        FOREIGN KEY (`client_id`)
-            REFERENCES `clients` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
-
-) engine InnoDB;
-
-create table if not exists order_s_goods
-(
-    id int unsigned not null auto_increment,
-# todo  пересчитать сумму заказа при изменении строки
-
-    orders_id int unsigned not null,
-    amount numeric(10,3)  default 0,
-    price numeric(15,2)  default 0,
-    row_total numeric(15,2)  default 0,
-    primary key (id),
-    index orders_id_index (orders_id),
-
-);*/
-
 drop schema if exists magazin;
 
 create schema if not exists magazin;
@@ -95,29 +27,143 @@ create table orders
         foreign key (client_id) references clients (id)
 ) comment 'Заказы';
 
+create table magazin.orders_debt
+(
+    id       int unsigned auto_increment,
+    order_id int unsigned not null,
+    debt     int          not null,
+    constraint orders_debt_pk
+        primary key (id),
+    constraint orders_debt_orders_id_fk
+        foreign key (order_id) references magazin.orders (id)
+)
+    comment 'Задолженность по заказам';
+
+
+create table type_of_goods
+(
+    id   int unsigned auto_increment,
+    name varchar(50) not null,
+    constraint type_of_goods_pk
+        primary key (id)
+) comment 'Виды номенклатуры';
+
+create unique index type_of_goods_name_uindex
+    on type_of_goods (name);
+
+create table spec_type_of_goods
+(
+    id               int unsigned auto_increment,
+    name             varchar(100) not null,
+    type_of_goods_id int unsigned not null,
+    primary key (id),
+    constraint spec_type_of_goods_type_of_goods_id_fk
+        foreign key (type_of_goods_id) references type_of_goods (id)
+)
+    comment 'Состав вида номенклатуры';
+
+create unique index spec_type_of_goods_name_type_of_goods_id_uindex
+    on spec_type_of_goods (name, type_of_goods_id);
+
+
+
+create table goods
+(
+    id               int unsigned auto_increment,
+    type_of_goods_id int unsigned not null,
+    name             varchar(150) not null,
+    constraint goods_pk
+        primary key (id),
+    constraint goods_type_of_goods_id_fk
+        foreign key (type_of_goods_id) references type_of_goods (id)
+)
+    comment 'Товары';
+
+create unique index goods_name_uindex
+    on goods (name);
+
+
+# 2.1 Склады
+create table warehouse
+(
+    id   int unsigned auto_increment,
+    name varchar(100) not null,
+    constraint warehouse_pk
+        primary key (id)
+) comment 'Склады';
+
+create unique index warehouse_name_uindex
+    on warehouse (name);
+
+create table leftover_goods
+(
+	warehouse_id int unsigned not null,
+	goods_id int unsigned not null,
+	received decimal(14,3) default 0 null,
+	shipped decimal(14,3) default 0 null,
+	data datetime default now() not null,
+	constraint leftover_goods_goods_id_fk
+		foreign key (goods_id) references goods (id),
+	constraint leftover_goods_warehouse_id_fk
+		foreign key (warehouse_id) references warehouse (id)
+)
+comment 'Движения товаров';
+
+create table values_of_spec_type
+(
+    id                   int unsigned auto_increment,
+    spec_type_of_good_id int unsigned not null,
+    `value`              varchar(100) not null,
+    constraint values_of_pk
+        primary key (id),
+    constraint values_of_spec_type_of_goods_id_fk
+        foreign key (spec_type_of_good_id) references spec_type_of_goods (id)
+) comment 'Значения характеристики вида номенклатуры';
+
+create unique index values_of_spec_type_of_good_id_value_uindex
+    on values_of_spec_type (spec_type_of_good_id, `value`);
+
+create table goods_description
+(
+    id                     int unsigned auto_increment,
+    goods_id               int unsigned not null,
+    spec_type_of_good_id   int unsigned not null,
+    values_of_spec_type_id int unsigned not null,
+    constraint goods_description_pk
+        primary key (id)
+) comment 'Возможные значения';
+
+create unique index goods_spec_values_uindex
+	on goods_description (goods_id, spec_type_of_good_id, values_of_spec_type_id);
+
+alter table goods_description
+    add constraint goods_description_goods_id_fk
+        foreign key (goods_id) references goods (id);
+
+alter table goods_description
+    add constraint goods_description_spec_type_of_goods_id_fk
+        foreign key (spec_type_of_good_id) references spec_type_of_goods (id);
+
+alter table goods_description
+    add constraint goods_description_values_of_spec_type_id_fk
+        foreign key (values_of_spec_type_id) references values_of_spec_type (id);
+
+
 # 3.2 Строки заказа
 create table order_rows
 (
-    id         int unsigned auto_increment,
-    order_id   int unsigned not null,
-    new_column int          null,
+    id       int unsigned auto_increment,
+    order_id int unsigned not null,
+    goods_id int unsigned null,
     constraint order_rows_pk
         primary key (id),
+    constraint order_rows_goods_id_fk
+        foreign key (goods_id) references goods (id),
     constraint order_rows_orders_id_fk
         foreign key (order_id) references orders (id)
-) comment 'Строки заказов';
-create table magazin.orders_debt
-
-(
-	id int unsigned auto_increment,
-	order_id int unsigned not null,
-	debt int not null,
-	constraint orders_debt_pk
-		primary key (id),
-	constraint orders_debt_orders_id_fk
-		foreign key (order_id) references magazin.orders (id)
 )
-comment 'Задолженность по заказам';
+    comment 'Строки заказов';
+
 
 # 4.1 Логирование
 create table logs
@@ -141,112 +187,6 @@ create index logs_order_id_index
 
 create index logs_order_rows_id_index
     on logs (order_rows_id);
-
-create table type_of_goods
-(
-    id   int unsigned auto_increment,
-    name varchar(50) not null,
-    constraint type_of_goods_pk
-        primary key (id)
-) comment 'Виды номенклатуры';
-
-create unique index type_of_goods_name_uindex
-    on type_of_goods (name);
-
-create table spec_type_of_goods
-(
-	id int unsigned auto_increment,
-	name varchar(100) not null,
-	type_of_goods_id int unsigned not null,
-	primary key (id),
-	constraint spec_type_of_goods_type_of_goods_id_fk
-		foreign key (type_of_goods_id) references type_of_goods (id)
-)
-comment 'Состав вида номенклатуры';
-
-create unique index spec_type_of_goods_name_uindex
-	on spec_type_of_goods (name);
-
-create table goods
-(
-    id               int unsigned auto_increment,
-    type_of_goods_id int unsigned not null,
-    constraint goods_pk
-        primary key (id),
-    constraint goods_type_of_goods_id_fk
-        foreign key (type_of_goods_id) references type_of_goods (id)
-)
-    comment 'Товары';
-
-
-# 2.1 Склады
-create table warehouse
-(
-    id   int unsigned auto_increment,
-    name varchar(100) not null,
-    constraint warehouse_pk
-        primary key (id)
-) comment 'Склады';
-
-create unique index warehouse_name_uindex
-    on warehouse (name);
-
-create table leftover_goods
-(
-    warehouse_id int unsigned             not null,
-    goods_id     int unsigned             not null,
-    received     decimal(14, 3) default 0 null,
-    shipped      decimal(14, 3) default 0 null,
-    constraint leftover_goods_goods_id_fk
-        foreign key (goods_id) references goods (id),
-    constraint leftover_goods_warehouse_id_fk
-        foreign key (warehouse_id) references warehouse (id)
-) comment 'Движения товаров';
-
-create table values_of_spec_type
-(
-	id int unsigned auto_increment,
-	spec_type_of_good_id int unsigned not null,
-	value varchar(100) not null,
-	constraint values_of_pk
-		primary key (id),
-	constraint values_of_spec_type_of_goods_id_fk
-		foreign key (spec_type_of_good_id) references spec_type_of_goods (id)
-) comment 'Значения характеристики вида номенклатуры';
-
-create unique index values_of_spec_type_of_good_id_value_uindex
-	on values_of_spec_type (spec_type_of_good_id, value);
-
-create table goods_description
-(
-	id int unsigned auto_increment,
-	goods_id int unsigned not null ,
-	spec_type_of_good_id int unsigned not null,
-	values_of_spec_type_id int unsigned not null,
-	constraint goods_description_pk
-		primary key (id)
-) comment 'Возможные значения';
-
-alter table goods_description
-	add constraint goods_description_goods_id_fk
-		foreign key (goods_id) references goods (id);
-
-alter table goods_description
-	add constraint goods_description_spec_type_of_goods_id_fk
-		foreign key (spec_type_of_good_id) references spec_type_of_goods (id);
-
-alter table goods_description
-	add constraint goods_description_values_of_spec_type_id_fk
-		foreign key (values_of_spec_type_id) references values_of_spec_type (id);
-
-
-
-
-
-
-
-
-
 
 
 
